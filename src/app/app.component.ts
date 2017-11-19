7797d6f0d5d80aacdbee53919636bfc6f3557228
Robby <ta7382@gmail.com>
2017-11-19 17:25:05 +0800

    install bootsrap and gmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgZone, OnInit, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule, MapsAPILoader, GoogleMapsAPIWrapper, AgmDataLayer } from '@agm/core';
 
+declare var google: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public title: string = 'GEO Converter';
+  public lat: number = 24.801577;
+  public lng: number = 120.971560;
+  public radius: number = 50000;
+
+  constructor(
+    private zone: NgZone,
+  ) { }
+
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    AgmCoreModule.forRoot({
+      apiKey: '',
+      language: 'zh-TW'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
